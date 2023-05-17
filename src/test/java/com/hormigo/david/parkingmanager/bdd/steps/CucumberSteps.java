223edc5b4647180b78ea65e8caf36b4fe8446e41
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-05-17 19:41:34 +0200

    Añadir tests parametrizados

--- src/test/java/com/hormigo/david/parkingmanager/bdd/steps/CucumberSteps.java
+++ src/test/java/com/hormigo/david/parkingmanager/bdd/steps/CucumberSteps.java
@@ -1,6 +1,5 @@
 package com.hormigo.david.parkingmanager.bdd.steps;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -8,6 +7,9 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.mockito.InjectMocks;
 import org.openqa.selenium.By;
@@ -19,14 +21,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.hormigo.david.parkingmanager.bdd.CucumberConfiguration;
+import com.hormigo.david.parkingmanager.user.domain.Role;
 import com.hormigo.david.parkingmanager.user.domain.User;
 import com.hormigo.david.parkingmanager.user.domain.UserRepository;
 import com.hormigo.david.parkingmanager.user.service.UserService;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
@@ -37,87 +45,100 @@ public class CucumberSteps extends CucumberConfiguration {
     @InjectMocks
     private UserService mockedUserService = mock(UserService.class);
     @Value("${local.server.port}")
-    private  int port;
+    private int port;
     private static ChromeDriver driver;
+
     @BeforeAll
     public static void prepareWebDriver() {
-        System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+
+    }
+
+    @Before
+    public void createDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
-        
     }
 
-    @Given("un usuario esta en la pagina inicial")
-    public void openHome() {
-        driver.get("http://localhost:" + port + "/");
-
-
+    @After
+    public void quitDriver() {
+        driver.quit();
     }
 
-    @Given("un administrador esta en el formulario de creación")
-    public void openUserCreateForm()
-    {
-        driver.get("http://localhost:" + port + "/newUser");
-    }
-
-    @Given("el correo no esta asignado a otro usuario")
-    public void mockUserDoesNotExists(){
-        when(mockedUserService.userExists(anyString())).thenReturn(false);
-    }
-
-    @Given("relleno el campo {} con {}") 
-    public void fillField(String fieldName,String value) {
-
-        WebElement field = driver.findElement(By.id(getFieldIdFromName(fieldName)));
-        field.sendKeys(value);
+    @Dado("un usuario esta en la pagina {}")
+    public void openPage(String pageName) {
+        driver.get(getUrlFromPageName(pageName));
 
     }
 
-    private String getFieldIdFromName(String name){
-        String id = "";
-        switch (name) {
-            case "correo":
-                id = "user-create-field-email";
+    private String getUrlFromPageName(String pageName) {
+        String endPoint = "";
+        switch (pageName) {
+            case "inicial":
+                endPoint = "/";
                 break;
-            case "nombre":
-                id = "user-create-field-name";
+            case "lista de usuarios":
+                endPoint = "/users";
                 break;
-            case "primer apellido":
-                id = "user-create-field-lastname1";
+            case "lista de sorteos":
+                endPoint = "/draws";
                 break;
-            case "segundo apellido":
-                id = "user-create-field-lastname2";
-                break;
-            case "crear usuario":
-                id = "user-create-button-submit";
-                break;
+            case "creación de usuarios":
+                endPoint = "/newUser";
+            break;
             default:
                 break;
         }
-        return id;
+        return getUrlFromEndPoint(endPoint);
     }
 
+    @Cuando("el usuario hace click sobre el botón de {}")
+    public void clickButton(String buttonName) {
+        String buttonId = "";
+        switch (buttonName) {
+            case "Usuarios":
+                buttonId = "to-users-link";
+                break;
+            case "Sorteos":
+                buttonId = "to-draws-link";
+                break;
 
-    @When("el usuario hace click sobre el botón de Usuarios")
-    public void clickUserButton(){
-        driver.findElement(By.id("to-users-link")).click();
+            default:
+                break;
+        }
+        driver.findElement(By.id(buttonId)).click();
+    }
 
+    @Entonces("esta en la pagina de {}")
+    public void isInPage(String pageName) {
+
+        assertTrue(driver.getCurrentUrl().equals(getUrlFromPageName(pageName)));
     }
-    @When("pulso el botón de {}")
-    public void clickElement(String element){
-        driver.findElement(By.id(getFieldIdFromName(element))).click();
-    }
-    @Then("se muestra la lista de usuarios")
-    public void navigateToUsersList(){
+
+    @Entonces("se muestra un campo de {}")
+    public void fieldIsDisplayed(String fieldName){
+        String fieldId = getFieldIdFromName(fieldName);
         String currentUrl = driver.getCurrentUrl();
-        assertTrue(currentUrl.contains("/users"));
+        WebElement field = driver.findElement(By.id(fieldId));
         
-    }
-    @Then("se ha persistido el usuario en la base de datos")
-    public void saveHasBeenCalled(){
-        verify(mockedRepository).save(any(User.class));
+        assertTrue(field.isDisplayed());
     }
 
+    private String getFieldIdFromName(String fieldName) {
+        String fieldId ="";
+        switch (fieldName) {
+            case "correo electrónico":
+                fieldId = "user-create-field-email";
+                break;
+        
+            default:
+                break;
+        }
+        return fieldId;
+    }
+    private String getUrlFromEndPoint(String endpoint) {
+        return "http://localhost:" + port + endpoint;
+    }
 
 }
