791f274240cf4df5697027e7d64a5ac9f9e1b297
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-05-24 19:51:59 +0200

    Test de GEt

---
+++ src/test/java/com/hormigo/david/parkingmanager/user/controller/UserControllerTest.java
@@ -0,0 +1,50 @@
+package com.hormigo.david.parkingmanager.user.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hormigo.david.parkingmanager.user.domain.Role;
+import com.hormigo.david.parkingmanager.user.domain.User;
+import com.hormigo.david.parkingmanager.user.service.UserService;
+import com.hormigo.david.parkingmanager.user.service.UserServiceImpl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Optional;
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+
+
+    @Test
+    public void testSingleUserRead() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        User user = new User("dhorram948@g.educaand.es","David","Hormigo","Ramírez",Role.PROFESSOR);
+        String json = mapper.writeValueAsString(user);
+        when(userService.getUser(2)).thenReturn(Optional.of(user));
+        this.mockMvc.perform(get("/api/users/2"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().json(json));
+                    
+
+    }
+}
